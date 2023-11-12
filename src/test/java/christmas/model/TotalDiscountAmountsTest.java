d1a31cc7039e21ce3b3aa06c3b283eb82b37539c
soochan <tncksdl05@gmail.com>
2023-11-12 23:47:43 +0900

    test(TotalDiscountAmounts): 총할인 혜택 금액 테스트 추가
    총할인 금액 생성 테스트 추가
    총할인 금액이 최소 요건을 만족하는지 테스트 추가
    총할인 금액이 VO 객체로 관리되는지 테스트 추가

---
+++ src/test/java/christmas/model/TotalDiscountAmountsTest.java
@@ -0,0 +1,77 @@
+package christmas.model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class TotalDiscountAmountsTest {
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1_000})
+    void 총할인_금액은_0원_이상을_가지면_예외가_발생하지_않는다(int amounts) {
+        assertDoesNotThrow(() -> TotalDiscountAmounts.from(amounts));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {-1, -1000})
+    void 총할인_금액이_0원_보다_작은_값을_가지면_예외가_발생한다(int amounts) {
+        assertThatThrownBy(() -> TotalDiscountAmounts.from(amounts))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 총할인_금액을_총주문_금액에서_차감할_수_있다() {
+        TotalDiscountAmounts totalDiscountAmounts = TotalDiscountAmounts.from(1_000);
+        int orderAmounts = 10_000;
+
+        int orderAmountsAfterDiscount = totalDiscountAmounts.calculateOrderAmountsAfterDiscount(orderAmounts);
+
+        assertThat(orderAmountsAfterDiscount).isEqualTo(9_000);
+    }
+
+    @Test
+    void 총할인_금액이_최소_자격요건을_만족하면_참을_반환한다() {
+        TotalDiscountAmounts totalDiscountAmounts = TotalDiscountAmounts.from(10_000);
+
+        boolean isEligible = totalDiscountAmounts.isEligibleFor(10_000);
+
+        assertThat(isEligible).isTrue();
+    }
+
+    @Test
+    void 총할인_금액이_최소_자격요건을_만족하지_않으면_거짓을_반환한다() {
+        TotalDiscountAmounts totalDiscountAmounts = TotalDiscountAmounts.from(9_999);
+
+        boolean isEligible = totalDiscountAmounts.isEligibleFor(10_000);
+
+        assertThat(isEligible).isFalse();
+    }
+
+    @Test
+    void 총할인_금액이_같으면_서로_같은_객체이다() {
+        TotalDiscountAmounts totalDiscountAmounts = TotalDiscountAmounts.from(10_000);
+        TotalDiscountAmounts sameTotalDiscountAmounts = TotalDiscountAmounts.from(10_000);
+
+        assertThat(totalDiscountAmounts).isEqualTo(sameTotalDiscountAmounts);
+    }
+
+    @Test
+    void 총할인_금액이_다르면_서로_다른_객체이다() {
+        TotalDiscountAmounts totalDiscountAmounts = TotalDiscountAmounts.from(10_000);
+        TotalDiscountAmounts differentTotalDiscountAmounts = TotalDiscountAmounts.from(9_999);
+
+        assertThat(totalDiscountAmounts).isNotEqualTo(differentTotalDiscountAmounts);
+    }
+
+    @Test
+    void 총할인_금액이_같으면_서로_같은_해시코드를_가진다() {
+        TotalDiscountAmounts totalDiscountAmounts = TotalDiscountAmounts.from(10_000);
+        TotalDiscountAmounts sameTotalDiscountAmounts = TotalDiscountAmounts.from(10_000);
+
+        assertThat(totalDiscountAmounts).hasSameHashCodeAs(sameTotalDiscountAmounts);
+    }
+}
