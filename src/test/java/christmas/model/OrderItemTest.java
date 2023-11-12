1450e3fd00e6ca9c3457016ac3561f13bde02983
soochan <tncksdl05@gmail.com>
2023-11-12 21:00:28 +0900

    test(OrderItem): 주문 품목 테스트 추가
    주문 항목 생성 테스트 추가
    주문 항목을 통해 금액 계산 테스트 추가
    주문 항목이 카테고리에 속하는지 테스트 추가
    주문 항목이 음료 카테고리에 속하는지 테스트 추가

---
+++ src/test/java/christmas/model/OrderItemTest.java
@@ -0,0 +1,114 @@
+package christmas.model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class OrderItemTest {
+
+    @Test
+    void 사용자_주문_항목_입력_값으로_주문_항목을_생성하는_경우_존재하지_않는_메뉴_이름이면_예외가_발생한다() {
+        OrderItemMapper nonExistentMenuOfOrderItemMapper = new OrderItemMapper("없는 메뉴", 1);
+
+        assertThatThrownBy(() -> OrderItem.from(nonExistentMenuOfOrderItemMapper))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, -1})
+    void 사용자_주문_항목_입력_값으로_주문_항목을_생성하는_경우_음수_및_0값이면_예외가_발생한다(int quantity) {
+        OrderItemMapper negativeQuantityOfOrderItemMapper = new OrderItemMapper("초코케이크", quantity);
+
+        assertThatThrownBy(() -> OrderItem.from(negativeQuantityOfOrderItemMapper))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 사용자_주문_항목_입력_값으로_올바른_메뉴와_수량으로_주문_항목을_생성하는_경우_예외가_발생하지_않는다() {
+        OrderItemMapper orderItemMapper = new OrderItemMapper("초코케이크", 1);
+
+        assertDoesNotThrow(() -> OrderItem.from(orderItemMapper));
+    }
+
+    @ParameterizedTest
+    @MethodSource("메뉴_수량_예상금액")
+    void 주문_항목이_가지는_금액을_계산할_수_있다(Menu menu, int quantity, int expectedTotal) {
+        OrderItem orderItem = new OrderItem(menu, Quantity.from(quantity));
+
+        int totalAmount = orderItem.calculateAmounts();
+
+        assertThat(totalAmount).isEqualTo(expectedTotal);
+    }
+
+    @ParameterizedTest
+    @MethodSource("메뉴_메뉴와일치하는카테고리")
+    void 주문_항목이_카테고리에_속하는_경우는_참을_응답한다(Menu menu, Category category) {
+        OrderItem orderItem = new OrderItem(menu, Quantity.from(1));
+
+        boolean belongsToCategory = orderItem.belongsTo(category);
+
+        assertThat(belongsToCategory).isTrue();
+    }
+
+    @ParameterizedTest
+    @MethodSource("메뉴_메뉴와일치하지않는카테고리")
+    void 주문_항목이_카테고리에_속하지_않는_경우는_거짓을_응답한다(Menu menu, Category category) {
+        OrderItem orderItem = new OrderItem(menu, Quantity.from(1));
+
+        boolean belongsToCategory = orderItem.belongsTo(category);
+
+        assertThat(belongsToCategory).isFalse();
+    }
+
+    @Test
+    void 주문_항목이_음료_카테고리에_속하는_경우_참을_응답한다() {
+        OrderItem orderItem = new OrderItem(Menu.RED_WINE, Quantity.from(1));
+
+        boolean isBeverage = orderItem.isBeverage();
+
+        assertThat(isBeverage).isTrue();
+    }
+
+    @Test
+    void 주문_항목이_음료_카테고리에_속하지_않는_경우_거짓을_응답한다() {
+        OrderItem orderItem = new OrderItem(Menu.MUSHROOM_SOUP, Quantity.from(1));
+
+        boolean isBeverage = orderItem.isBeverage();
+
+        assertThat(isBeverage).isFalse();
+    }
+
+    private static Stream<Arguments> 메뉴_메뉴와일치하지않는카테고리() {
+        return Stream.of(
+                Arguments.of(Menu.MUSHROOM_SOUP, Category.MAIN),
+                Arguments.of(Menu.T_BONE_STEAK, Category.DESSERT),
+                Arguments.of(Menu.CHOCOLATE_CAKE, Category.BEVERAGE),
+                Arguments.of(Menu.RED_WINE, Category.APPETIZER)
+        );
+    }
+
+    private static Stream<Arguments> 메뉴_메뉴와일치하는카테고리() {
+        return Stream.of(
+                Arguments.of(Menu.MUSHROOM_SOUP, Category.APPETIZER),
+                Arguments.of(Menu.T_BONE_STEAK, Category.MAIN),
+                Arguments.of(Menu.CHOCOLATE_CAKE, Category.DESSERT),
+                Arguments.of(Menu.RED_WINE, Category.BEVERAGE)
+        );
+    }
+
+    private static Stream<Arguments> 메뉴_수량_예상금액() {
+        return Stream.of(
+                Arguments.of(Menu.MUSHROOM_SOUP, 1, 6_000),
+                Arguments.of(Menu.T_BONE_STEAK, 2, 110_000),
+                Arguments.of(Menu.ICE_CREAM, 3, 15_000)
+        );
+    }
+
+}
